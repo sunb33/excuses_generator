aea809e9359f53061f5405206199cc7aeb60ece0
sunb33 <behb233@gmail.com>
2020-08-03 00:21:28 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,22 @@
-/* eslint-disable */
+window.onload = () => {
+  document.querySelector("#excusita").innerHTML = excs();
 
-import "../assets/img/rigo-baby.jpg";
-import "../assets/img/4geeks.ico";
-//import 'breathecode-dom'; //DOM override to make JS easier to use
-import "../style/index.scss";
-
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  console.log(random());
 };
+
+function excs() {
+  let who = ["my dog", "my dad", "my mom", "my sis"];
+  let what = ["crush", "broke", "eat", "lost"];
+  let whatd = ["my pizza", "my lapto", "the car", "my doritos"];
+  let when = ["yesterday", "at nigth", "today", "when i was sleeping"];
+
+  let whos = Math.floor(Math.random() * who.length);
+  let whats = Math.floor(Math.random() * what.length);
+  let whatds = Math.floor(Math.random() * whatd.length);
+
+  let whens = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whos] + " " + what[whats] + " " + whatd[whatds] + " " + when[whens]
+  );
+}
